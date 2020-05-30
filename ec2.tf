ab428bb96840d0bae58ada51374c951927b29362
Ashok Jeevan <ajeevan@stti.ca>
2020-05-30 00:37:17 -0700

    Added variables.tf which is being referred by ec2.tf for creating infra

--- ec2.tf
+++ ec2.tf
@@ -1,4 +1,48 @@
-resource "aws_instance" "example" {
-  ami           = "ami-0f75c2980c6a5851d"
-  instance_type = "t2.micro"
+provider "aws" {
+  region = "${var.aws_region}"
 }
+
+# VPC
+resource "aws_vpc" "terraformVPC" {
+  cidr_block = "${var.vpc_cidr_range}"
+  instance_tenancy = "default"
+  tags = {
+    Name = "Terraform VPC"
+    Location = "canada"
+  }
+}
+
+# Public Subnet
+resource "aws_subnet" "terrformPublicSubnet" {
+  vpc_id = "${aws_vpc.terraformVPC.id}"
+  availability_zone = "ca-central-1a"
+  cidr_block = "${var.public_subnet_cidr}"
+  tags = {
+    Name = "Public Subnet"
+  }
+}
+
+# Private Subnet
+resource "aws_subnet" "terraformPrivateSubnet" {
+  vpc_id = "${aws_vpc.terraformVPC.id}"
+  availability_zone = "ca-central-1b"
+  cidr_block = "${var.private_subnet_cidr}"
+  tags = {
+    Name = "Private Subnet"
+  }
+}
+
+# Internet Gateway to connect to the VPC
+/* resource "aws_internet_gateway" "igw" {
+  vpc_id = "${aws_vpc.myVPC.id}"
+}
+
+resource "aws_instance" "myFirstInstance" {
+  ami           = "${lookup(var.aws_ami, var.aws_region)}"
+  instance_type = "t2.micro"
+  tags {
+    Name = "Web Server"
+  }
+  # hibernation = true - for this to work, the root volume should be encrypted
+  # by default, the root volume is not encrypted
+} */
---
+++ variables.tf
@@ -0,0 +1,27 @@
+variable "aws_region" {
+    description = "AWS region to launch servers"
+    default = "ca-central-1"
+}
+
+# Linux based amis
+variable "aws_ami" {
+    default = {
+        ca-central-1 = "ami-0f75c2980c6a5851d"
+    }
+}
+
+# VPC CIDR
+variable "vpc_cidr_range" {
+    default = "10.0.0.0/16"
+}
+
+# Public Subnet CIDR
+variable "public_subnet_cidr" {
+    default = "10.0.1.0/24"
+}
+
+# Private Subnet CIDR
+variable "private_subnet_cidr" {
+    default = "10.0.2.0/24"
+}
+
